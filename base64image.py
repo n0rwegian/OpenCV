1267726ecaf0e2fcd5391b7ed072bd0028d65e79
N0rwegian <f1orano@yandex.ru>
2024-10-05 16:03:56 +0700

    base64 image pixel colours

---
+++ base64image.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import base64
+
+colors = {(255,255,255):"white",
+          (0,0,255):"red",
+          (0,255,0):"green",
+          (255,0,0):"blue",
+          (0,255,255):"yellow",
+          (255,0,255):"purple",
+          (255,255,0):"aqua",
+          (0,0,0):"black",
+          (128,128,128):"gray",}
+
+def read_image(input_text):
+    img = cv2.imdecode(np.frombuffer(base64.b64decode(input_text), dtype=np.uint8), cv2.IMREAD_COLOR)
+    return img
+
+
+image = read_image(input())
+x, y = map(int, input().split())
+print(colors[tuple(image[y, x])])
